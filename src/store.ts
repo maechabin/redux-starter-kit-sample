9aca1052c608a00ab267df59c854498d4fdadba4
maechabin <mitoppo@gmail.com>
2019-06-29 16:55:14 +0900

    middleware対応

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,19 @@
-import { combineReducers, configureStore } from 'redux-starter-kit';
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from 'redux-starter-kit';
 
 import appModule from './modules/appModule';
-
+// reducer
 const reducer = combineReducers({
   app: appModule.reducer,
 });
 
+// middleware
+const middlewares = [...getDefaultMiddleware()];
+
 export const store = configureStore({
   reducer,
+  middleware: middlewares,
 });
